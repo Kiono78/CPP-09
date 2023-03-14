414fe832b114189f83911cdd25b60e35b8f4829b
Kiono78 <benoit.terral@gmail.com>
2023-03-14 13:48:22 +0100

    starting 00

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,31 @@
+#include "../headers/BitcoinExchange.hpp"
+
+int main(int argc, char **argv) {
+	(void)argv;
+	std::ifstream file("data.csv");
+	if (argc != 2) {
+		std::cout << "Input file expected" << std::endl;
+		return 1;
+	}
+	if (file.fail()) {
+		std::cout << "Data file loading failed" << std::endl;
+		return 1;
+	}
+	else if (file.is_open()) {
+		std::string	line;
+		while (std::getline(file, line)) {
+			// std::cout << line << std::endl;
+			std::stringstream	ss(line);
+			while (ss.good()) {
+				std::cout << ss << std::endl;
+			}
+		}
+		file.close();
+	}
+	else
+	{
+		std::cout << "Unknown error" << std::endl;
+		return 1;
+	}
+	return 0;
+}
