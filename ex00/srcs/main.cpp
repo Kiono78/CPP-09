40812e6a9719891f7859ee6db677d63019fcbf16
Kiono78 <benoit.terral@gmail.com>
2023-03-14 14:19:41 +0100

    ex00 map filled

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char **argv) {
 	(void)argv;
 	std::ifstream file("data.csv");
+	std::map<std::string, std::string>	data;
 	if (argc != 2) {
 		std::cout << "Input file expected" << std::endl;
 		return 1;
@@ -12,16 +13,10 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	else if (file.is_open()) {
-		std::map<std::string, std::string>	data;
 		std::string							line;
 		while (std::getline(file, line)) {
-			// std::cout << line << std::endl;
 			std::stringstream	ss(line);
-			while (ss.good()) {
-				std::string field;
-				std::getline(ss, field, ',');
-				std::cout << line << std::endl;
-			}
+			data[line.substr(0, line.find(','))] = line.substr(line.find(',') + 1, line.length());
 		}
 		file.close();
 	}
@@ -30,5 +25,8 @@ int main(int argc, char **argv) {
 		std::cout << "Unknown error" << std::endl;
 		return 1;
 	}
+	std::map<std::string, std::string>::iterator end = data.end();
+	for (std::map<std::string, std::string>::iterator it = data.begin(); it != end; it++)
+		std::cout << it->first << " " << it->second << std::endl;
 	return 0;
 }
